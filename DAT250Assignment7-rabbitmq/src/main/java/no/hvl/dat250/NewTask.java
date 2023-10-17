b45e58b196839afb03701965af3c0cf7f31bd600
Lars Martin Taraldset <Lars.Taraldset@student.uib.no>
2023-10-17 20:10:56 +0200

    complete tutoril

--- DAT250Assignment7-rabbitmq/src/main/java/no/hvl/dat250/NewTask.java
+++ DAT250Assignment7-rabbitmq/src/main/java/no/hvl/dat250/NewTask.java
@@ -5,26 +5,24 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.MessageProperties;
 
-import java.nio.charset.StandardCharsets;
-import java.util.Scanner;
+
 public class NewTask {
 
-    private final static String QUEUE_NAME = "task_queue";
+    private static final String TASK_QUEUE_NAME = "task_queue";
 
     public static void main(String[] argv) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         try (Connection connection = factory.newConnection();
              Channel channel = connection.createChannel()) {
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
 
-            String[] messages = {"First message....", "Second message....................", "Third message....", "Fourth message...", "Fifth message."};
-            for(String message: messages){
-                channel.basicPublish("", QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes());
-                System.out.println(" [x] Sent '" + message + "'");
-            }
+            String message = String.join(" ", argv);
+
+            channel.basicPublish("", TASK_QUEUE_NAME,
+                    MessageProperties.PERSISTENT_TEXT_PLAIN,
+                    message.getBytes("UTF-8"));
+            System.out.println(" [x] Sent '" + message + "'");
         }
-
-
     }
 }
